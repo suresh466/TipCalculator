b4252c37baa67f3b2ba43f05cfa9ec0cadd88a32
Suresh <suresh.thagunna77@gmail.com>
2024-05-31 16:59:10 -0400

    create tipclculator model view and controller

--- TipCalculator/Controllers/HomeController.cs
+++ TipCalculator/Controllers/HomeController.cs
@@ -13,11 +13,32 @@ namespace TipCalculator.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Tip15 = 0;
+            ViewBag.Tip20 = 0;
+            ViewBag.Tip25 = 0;
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Tip15 = model.CalculateTip(15);
+                ViewBag.Tip20 = model.CalculateTip(20);
+                ViewBag.Tip25 = model.CalculateTip(25);
+            }
+            else
+            {
+                ViewBag.Tip15 = 0;
+                ViewBag.Tip20 = 0;
+                ViewBag.Tip25 = 0;
+            }
+            return View(model);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ TipCalculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage="Cost of meal is requried!")]
+        [Range(1, double.MaxValue, ErrorMessage="Cost of meal must be greater than 0")]
+        public decimal CostOfMeal { get; set; }
+
+        public decimal CalculateTip(decimal percentage)
+        {
+            return CostOfMeal * (percentage / 100);
+        }
+    }
+
+
+}
